bd07596f05c56a7c03a4a3c19444a438caaf37b4
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-27 19:10:41 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,8 +2,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// функція, яка виконує запит на сервер
-// import axios from 'axios';
+import axios from 'axios';
 export async function fetchImages(query) {
   const url = 'https://pixabay.com/api/';
   const params = new URLSearchParams({
@@ -13,25 +12,6 @@ export async function fetchImages(query) {
     orientation: 'horizontal',
     safesearch: true,
   });
-  // const options = {
-  //   method: 'GET',
-  // };
-  // return fetch(url, options).then(res => res.json());
   const res = await axios.get(url, { params });
   return res.data;
-  // .then(response => {
-  //   if (!response.ok) {
-  //     throw new Error(response.status);
-  //   }
-  //   return response.json();
-  // })
-  // .catch(error => {
-  //   iziToast.error({
-  //     backgroundColor: 'red',
-  //     position: 'topCenter',
-  //     title: 'Error',
-  //     message:
-  //       'Sorry, there are no images matching your search query. Please try again!',
-  //   });
-  // });
 }
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -2,36 +2,59 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchImages } from '../src/js/pixabay-api';
-import { renderImages } from './js/render-function';
-export const refs = {
-  formEl: document.querySelector('form'),
-  formCont: document.querySelector('form-container'),
-  inputEl: document.querySelector('input.form-control'),
-  ulEl: document.querySelector('ul.gallery'),
-};
-export let query = '';
-refs.inputEl.addEventListener('input', e => {
-  e.preventDefault();
-  query = refs.inputEl.value.trim();
+import { refs } from '../main';
+
+export async function renderImages(data) {
   refs.ulEl.innerHTML = '';
-});
-const fetchUsersBtn = document.querySelector('.btn');
-fetchUsersBtn.addEventListener('click', e => {
-  e.preventDefault();
-  if (query) {
-    refs.ulEl.innerHTML = '<div class="loader"></div>';
-    fetchImages(query)
-      .then(data => renderImages(data))
-      .catch(error => {
-        console.log(error);
-        iziToast.error({
-          title: 'Error',
-          backgroundColor: 'red',
-          position: 'topRight',
-          message:
-            'Please Sorry, there are no images matching your search query. Please try again! again!',
-        });
-      });
+  console.log(data);
+  const images = data.hits;
+  // console.log(data.hits);
+  if (images.length == 0) {
+    iziToast.error({
+      title: 'Error',
+      message: `Error: No such pictures!`,
+      position: 'topRight',
+    });
   }
-});
+
+  refs.ulEl.insertAdjacentHTML('beforeend', createGalleryMarkup(images));
+  const galleryCfg = {
+    captionsData: 'alt',
+    captionDelay: 250,
+  };
+  let lightbox = new SimpleLightbox('.gallery a', galleryCfg);
+  lightbox.on('show.simplelightbox', function () {});
+  lightbox.refresh();
+}
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+        <img       
+          class="gallery-image"
+          src="${webformatURL}"
+          alt="${tags}"
+        />
+        <ul class="img-description"><li class><h3>Likes</h3><p>${likes}</p><li/>
+        <li><h3>Views</h3><p>${views}</p><li/>
+        <li><h3>Comments</h3><p>${comments}</p><li/>  
+        <li><h3>Downloads</h3><p>${downloads}</p><li/></ul>
+      </a>
+    </li>
+     
+  `
+    )
+    .join('');
+}
+// container.insertAdjacentHTML('beforeend', createGalleryMarkup(images));
